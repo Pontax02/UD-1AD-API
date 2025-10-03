43d766db2a2d9f2d6b5cfae01116a7ffc8f83204
Pontax02 <ppontanilla07@gmail.com>
2025-10-03 13:25:36 +0200

    create classes based on json

---
+++ src/main/java/pablo/api/Info.java
@@ -0,0 +1,45 @@
+package pablo.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Info{
+
+	@JsonProperty("next")
+	private String next;
+
+	@JsonProperty("pages")
+	private int pages;
+
+	@JsonProperty("prev")
+	private Object prev;
+
+	@JsonProperty("count")
+	private int count;
+
+	public String getNext(){
+		return next;
+	}
+
+	public int getPages(){
+		return pages;
+	}
+
+	public Object getPrev(){
+		return prev;
+	}
+
+	public int getCount(){
+		return count;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Info{" + 
+			"next = '" + next + '\'' + 
+			",pages = '" + pages + '\'' + 
+			",prev = '" + prev + '\'' + 
+			",count = '" + count + '\'' + 
+			"}";
+		}
+}
---
+++ src/main/java/pablo/api/Location.java
@@ -0,0 +1,29 @@
+package pablo.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Location{
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("url")
+	private String url;
+
+	public String getName(){
+		return name;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Location{" + 
+			"name = '" + name + '\'' + 
+			",url = '" + url + '\'' + 
+			"}";
+		}
+}
